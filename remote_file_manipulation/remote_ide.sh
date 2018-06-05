0fff1298f0867efb12e7c0abad93c363b2e26617
Mateus Sousa <mateus.aluufc@gmail.com>
2018-06-04 23:05:53 -0300

    Adding remote file manipulation scripts

---
+++ remote_file_manipulation/remote_ide.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Imports
+source ssh_functions.sh
+
+# Avoiding trojans
+alias ssh="$(/usr/bin/which ssh)"
+alias zenity="$(/usr/bin/which zenity)"
+
+# Colors
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # no color
+
+main(){
+  if [[ $(test_connection) = "0" ]]; then
+  file_name=$(zenity --entry --title "File selection" --text "Enter a file name or some regex" --width 300 --height 100)
+  chosen_ones=$(zenity --list\
+  --checklist\
+  --title="Options"\
+  --text="Select the files to edit" \
+  --width 500 --height 400\
+  --column="Files" \
+  --column="" $(ssh_cmd "locate $file_name") | sed 's/^$//g')
+  # Handle zenity output
+  chosen_ones="$(sed 's/|/\n/g' <<< "$chosen_ones")"
+  # Is empty?
+  [ "${chosen_ones}" ] && ssh_download "$chosen_ones"
+  else
+      # printf "${RED}Error on connection!${NC}\n"
+      zenity --error --text "Error on connection!" --width 100 --height 50
+  fi
+}
+
+user=$(zenity --entry --title "User" --text "User name" --width 300 --height 100)
+addr=$(zenity --entry --title "Host" --text "Host address" --width 300 --height 100)
+
+main $user "$addr"
---
+++ remote_file_manipulation/ssh_functions.sh
@@ -0,0 +1,29 @@
+# This script:
+#   provide useful functions for remote_ide.sh
+#
+#
+# Auth Infos
+user=""
+addr=""
+
+test_connection(){
+  /usr/bin/ssh-copy-id "$user@$addr"
+  ssh "$user@$addr" "ls" > /dev/null
+  echo $?
+}
+
+ssh_cmd(){
+  ssh "$user@$addr" "$1"
+}
+
+ssh_download(){
+  arr="$1"
+  zenity --text-info --auto-scroll <<< $(
+    for f in ${arr[@]}
+    do
+      save="$(awk -F'/' '{print $NF}' <<< "$f")"
+      echo -e "Downloading $save..."
+      scp "$user@${addr}:$f" "$save"
+    done
+  )
+}
