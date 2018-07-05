bd96ffa6c89c26570b93b51da424357ef17e82ea
Mateus Sousa <mateus.aluufc@gmail.com>
2018-07-04 22:32:54 -0300

    New script

--- close_unauthorized_session/gatekeeper.py
+++ close_unauthorized_session/gatekeeper.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
-import Tkinter
+from Tkinter import *
+from threading import Thread
 import os,time
 
+p = Tk()
+Clicks = 0
+
 def gatekeeper():
-    p = Tkinter.Tk()
-    initial_pos = p.winfo_pointerxy()
-    while 1:
-        posxy = p.winfo_pointerx()
-        if posxy != initial_pos:
-            os.system("gnome-session-quit --force")
-        time.sleep(0.5)
+    time.sleep(15)
+    try:
+        initial_pos = p.winfo_pointerxy()
+        while 1:
+            posxy = p.winfo_pointerxy()
+            if posxy != initial_pos:
+                os.system("gnome-session-quit --force")
+            time.sleep(0.5)
+    except:
+           os.system("gnome-session-quit --force")
+
+
+def check_clicks(event):
+    global Clicks
+    if Clicks == 5:
+        print "You're welcome!"
+        exit(-1)
+    else:
+        Clicks+=1
+
+if __name__ == '__main__':
+    t = Thread(None, target=gatekeeper)
+    t.start()
+
+    widget = Button(None, text="Click-me or I'll close your session :)")
+    widget.pack()
+    widget.bind('<Button-1>', check_clicks)
+    widget.mainloop()
